ca5a46fb5820cc48f2384b4b292ce50eb248095b
guzman <jesseguzman@dal.ca>
2024-03-10 13:20:37 -0300

    Add set locale commands

--- dat_setup/ros_dependencies.sh
+++ dat_setup/ros_dependencies.sh
@@ -1,44 +1,14 @@
-# ensure homebrew is installed
-brew doctor
+# Set locale
 
-brew install python@3.8
-# Unlink in case you have python@3.7 installed already
-brew unlink python
-# Make the python command be Python 3.8
-brew link --force python@3.8
+# current locale
+locale  
 
-# install asio and tinyxml2 for Fast-RTPS
-brew install asio tinyxml2
+# test
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 
-# install dependencies for robot state publisher
-brew install tinyxml eigen pcre poco
+# updated locale
+locale 
 
-# OpenCV isn't a dependency of ROS 2, but it is used by some demos.
-brew install opencv
-
-# install OpenSSL for DDS-Security
-brew install openssl
-# if you are using ZSH, then replace '.bashrc' with '.zshrc'
-echo "export OPENSSL_ROOT_DIR=$(brew --prefix openssl)" >> ~/.bashrc
-
-# install Qt for RViz
-brew install qt freetype assimp
-
-# install console_bridge for rosbag2
-brew install console_bridge
-
-# install dependencies for rcl_logging_log4cxx
-brew install log4cxx spdlog
-
-# install CUnit for Cyclone DDS
-brew install cunit
-
-python3 -m pip install rosdistro
-brew install sip pyqt5
-ln -s /usr/local/share/sip/Qt5 /usr/local/share/sip/PyQt5
-brew install graphviz
-python3 -m pip install pygraphviz pydot
-python3 -m pip install lxml
-python3 -m pip install catkin_pkg empy ifcfg lark-parser lxml netifaces numpy pyparsing pyyaml setuptools argcomplete
-
-echo "Dependency installation complete!"
--- dat_setup/setup.py
+++ dat_setup/setup.py
@@ -1,8 +1,8 @@
 import os
 
-# install ROS 2 Foxy on Mac (x86-84)
-os.system ("sh ./dat_setup/ros_dependencies.sh")
-os.system("sh ./dat_setup/ros_install.sh")
+# install ROS 2 Foxy 
+os.system ("wsl ./ros_dependencies.sh")
+# os.system("sh ./dat_setup/ros_install.sh")
 
-# install Boost on Mac (x86-84)
-os.system("sh ./dat_setup/boost_install.sh")
+# install Boost
+# os.system("sh ./dat_setup/boost_install.sh")
